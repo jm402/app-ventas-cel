2851bfb7acfd94a067a1071c98d6090b25758bd2
Jahairo Acevedo <desarrollador27@dalton.com.mx>
2024-05-02 14:42:47 -0600

    fix

--- src/app/shared/services/menu.service.ts
+++ src/app/shared/services/menu.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable, map } from 'rxjs';
-import { CONEXION } from '../../conexion';
 
 @Injectable({
   providedIn: 'root'
